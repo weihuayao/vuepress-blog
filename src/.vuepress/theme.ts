bddb945f78a0783ea54bfeedb56f0d320ac9269d
wayde <wayde.yao@sailheader.com>
2023-04-03 15:26:15 +0800

    feat:"init my blog"

---
+++ src/.vuepress/theme.ts
@@ -0,0 +1,177 @@
+import { hopeTheme } from "vuepress-theme-hope";
+import {  zhNavbar } from "./navbar/index.js";
+import { enSidebar, zhSidebar } from "./sidebar/index.js";
+
+const MR_HOPE_AVATAR =
+  'https://avatars.githubusercontent.com/u/26462167?v=4'
+export default hopeTheme({
+  hostname: "https://mister-hope.github.io",
+
+  author: {
+    name: "wayde",
+    url: "https://mrhope.site",
+  },
+  iconAssets: "iconfont",
+  logo: "https://avatars.githubusercontent.com/u/26462167?v=4",
+  repo: "vuepress-theme-hope/vuepress-theme-hope",
+  docsDir: "docs",
+  blog: {
+    roundAvatar:true,
+    medias: {
+      Email: "903429061@qq.com",
+      GitHub: "https://github.com/weihuayao",
+      QQ: "903429061",
+      Wechat: "yao90342061",
+      wayde: ["https://mrhope.site", MR_HOPE_AVATAR],
+    },
+  },
+
+  locales: {
+    /**
+     * Chinese locale config
+     */
+    "/": {
+      // navbar
+      navbar: zhNavbar,
+
+      // sidebar
+      sidebar: zhSidebar,
+
+      footer: "默认页脚",
+
+      displayFooter: true,
+
+      blog: {
+        description: "一个前端开发者",
+        intro: "/zh/intro.html",
+      },
+
+      // page meta
+      metaLocales: {
+        editLink: "在 GitHub 上编辑此页",
+      },
+    },
+  },
+
+  encrypt: {
+    config: {
+      "/demo/encrypt.html": ["1234"],
+      "/zh/demo/encrypt.html": ["1234"],
+    },
+  },
+
+  navbarLayout: {
+    start: ['Brand'],
+    center: [],
+    end: ['Search', 'Links', 'Language', 'Repo', 'Outlook'],
+  },
+
+  plugins: {
+    blog: true,
+
+    comment: {
+      // @ts-expect-error: You should generate and use your own comment service
+      provider: "Giscus",
+    },
+
+    // all features are enabled for demo, only preserve features you need here
+    mdEnhance: {
+      align: true,
+      attrs: true,
+      chart: true,
+      codetabs: true,
+      demo: true,
+      echarts: true,
+      figure: true,
+      flowchart: true,
+      gfm: true,
+      imgLazyload: true,
+      imgSize: true,
+      include: true,
+      katex: true,
+      mark: true,
+      mermaid: true,
+      playground: {
+        presets: ["ts", "vue"],
+      },
+      presentation: {
+        plugins: ["highlight", "math", "search", "notes", "zoom"],
+      },
+      stylize: [
+        {
+          matcher: "Recommended",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommended",
+              };
+          },
+        },
+      ],
+      sub: true,
+      sup: true,
+      tabs: true,
+      vPre: true,
+      vuePlayground: true,
+    },
+
+    // uncomment these if you want a PWA
+    // pwa: {
+    //   favicon: "/favicon.ico",
+    //   cacheHTML: true,
+    //   cachePic: true,
+    //   appendBase: true,
+    //   apple: {
+    //     icon: "/assets/icon/apple-icon-152.png",
+    //     statusBarColor: "black",
+    //   },
+    //   msTile: {
+    //     image: "/assets/icon/ms-icon-144.png",
+    //     color: "#ffffff",
+    //   },
+    //   manifest: {
+    //     icons: [
+    //       {
+    //         src: "/assets/icon/chrome-mask-512.png",
+    //         sizes: "512x512",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-mask-192.png",
+    //         sizes: "192x192",
+    //         purpose: "maskable",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/assets/icon/chrome-192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //     shortcuts: [
+    //       {
+    //         name: "Demo",
+    //         short_name: "Demo",
+    //         url: "/demo/",
+    //         icons: [
+    //           {
+    //             src: "/assets/icon/guide-maskable.png",
+    //             sizes: "192x192",
+    //             purpose: "maskable",
+    //             type: "image/png",
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // },
+  },
+});
---
+++ src/README.md
@@ -0,0 +1,22 @@
+---
+home: true
+layout: BlogHome
+icon: home
+title: 主页
+heroImage: https://avatars.githubusercontent.com/u/26462167?v=4
+heroText: wayde
+tagline:  勿忘初心
+heroImageStyle : "border-radius:50%"
+description: 博客,记录,分享
+heroFullScreen: true
+projects:
+  - icon: link
+    name: 查看索引
+    desc: 站内文章索引和分类
+    link: /article
+
+  - icon: link
+    name: 友情链接
+    desc: 欢迎交换友情链接
+    link: /favorite/links/friend
+footer: 一个前端憨憨，上班只为下班。
